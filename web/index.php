22da19c8b98800820f84e27c5856f9073c66fa02
vic <victor.barba@toro.io>
2015-09-04 10:51:22 +0800

    Updated index.php

--- web/index.php
+++ web/index.php
@@ -38,7 +38,8 @@
 
 <div class="container">
   <div class="starter-template">
-    <h1>Bootstrap starter template</h1>
+    <h1>PSITE ROCKS!</h1>
+    <img src="http://www.psitereg3.org/psitereg3/images/psite.png"/>
     <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
   </div>
 </div>
